f08baef1571c1d0870efc48945dbf06361d3b9d7
Gustavo Luiz <glogustavo4321@gmail.com>
2022-11-17 20:34:24 -0300

    Create class connection BD

---
+++ workout-planner-web/src/main/java/Dao/JpaUtil.java
@@ -0,0 +1,24 @@
+package Dao;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class JpaUtil {
+
+	private static final String PERSISTENCE_UNIT_NAME = "pu";
+	private static EntityManagerFactory factory;
+
+	public static EntityManagerFactory getEntityManagerFactory() {
+		if (factory == null) {
+			factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		}
+		return factory;
+	}
+
+	public static void shutdown() {
+		if (factory != null) {
+			factory.close();
+		}
+	}
+	
+}
+
