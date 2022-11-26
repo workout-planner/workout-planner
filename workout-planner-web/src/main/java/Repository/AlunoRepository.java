a22a7349179eb4947e8c935d1b62590b7bab9fc5
Gustavo Luiz <glogustavo4321@gmail.com>
2022-11-26 14:07:39 -0300

    Update Personal and Aluno REPOSITORY
    Criação para retornar por CBO, MATRICULA e Id.
    Obs:Codigo pode ser reduzidos. Muita duplicação de código. LEMBRAR

--- workout-planner-web/src/main/java/Repository/AlunoRepository.java
+++ workout-planner-web/src/main/java/Repository/AlunoRepository.java
@@ -1,20 +1,17 @@
 package Repository;
 
-import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
 import Dao.Dao;
-import Dao.JpaUtil;
-import Models.Personal;
+import Models.Aluno;
 import Models.Pessoa;
 
-public class PersonalRepository extends Dao<Pessoa, Integer>
-{
-	public Pessoa getForCbo(int CBO, String senha) {
+public class AlunoRepository extends Dao<Pessoa, Integer>{
+	public Pessoa getForMatricula(String matricula, String senha) {
 		Pessoa usuario;
 		try {
-			TypedQuery<Pessoa> query = em.createQuery("SELECT e FROM Pessoa e WHERE e.CBO = ?1 and e.Senha = ?2", Pessoa.class);
-			query.setParameter(1, CBO);
+			TypedQuery<Pessoa> query = em.createQuery("SELECT e FROM Pessoa e WHERE e.matricula = ?1 and e.Senha = ?2", Pessoa.class);
+			query.setParameter(1, matricula);
 			query.setParameter(2, senha);
 			
 			usuario = query.getSingleResult();
@@ -25,11 +22,11 @@ public class PersonalRepository extends Dao<Pessoa, Integer>
 		return usuario;
 	}
 	
-	public Pessoa buscarPorCbo(int CBO) {
+	public Pessoa buscarPorMatricula(String matricula) {
 		Pessoa usuario;
 		try {
-			TypedQuery<Pessoa> query = em.createQuery("SELECT e FROM Pessoa e WHERE e.CBO = ?1", Pessoa.class);
-			query.setParameter(1, CBO);
+			TypedQuery<Pessoa> query = em.createQuery("SELECT e FROM Pessoa e WHERE e.matricula = ?1", Pessoa.class);
+			query.setParameter(1, matricula);
 			
 			usuario = query.getSingleResult();
 		} catch (Exception e) {
@@ -40,17 +37,16 @@ public class PersonalRepository extends Dao<Pessoa, Integer>
 	}
 	
 	public Pessoa buscarPorId(int id) {
-		Pessoa personal;
+		Pessoa aluno;
 		try {
 			TypedQuery<Pessoa> query = em.createQuery("SELECT e FROM Pessoa e WHERE e.Id = ?1", Pessoa.class);
 			query.setParameter(1, id);
 			
-			personal = query.getSingleResult();
+			aluno = query.getSingleResult();
 		} catch (Exception e) {
-			personal = null;
+			aluno = null;
 		}
 		
-		return personal;
+		return aluno;
 	}
-
 }
